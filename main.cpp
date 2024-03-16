bc40d0686a97a7891a40d94d1b4d5801ba98a12e
Rick D <78909973+rickd-uk@users.noreply.github.com>
2024-03-16 21:34:31 +0900

    add: class Grid

--- main.cpp
+++ main.cpp
@@ -4,7 +4,10 @@
 
 int main(void) {
   Color gray = {29, 29, 29, TRANSPARENCY};
-  InitWindow(800, 600, "Game Of Life");
+  const int cellSize = 25;
+  const int WIN_WIDTH = 750;
+  const int WIN_HEIGHT = 750;
+  InitWindow(WIN_WIDTH, WIN_HEIGHT, "Game Of Life");
 
   const int FPS = 12;
   SetTargetFPS(FPS);
